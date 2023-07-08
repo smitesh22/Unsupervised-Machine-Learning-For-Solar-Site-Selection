237f4ccf4944c8a536fad2c52748ccb1a4604ff1
smitesh22 <smitesh22@gmail.com>
2023-07-08 01:33:31 +0100

    dem.pycreated

---
+++ DEM.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import pandas as pd
+
+
+def create_csv():
+    pass
+
+
+if __name__ == "__main__":
+    print("yess")
